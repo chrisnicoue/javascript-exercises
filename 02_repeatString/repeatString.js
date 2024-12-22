9a5e0fdefc0fc5c3656d6f804e61f2b7b273f611
chrisnicoue <144281691+chrisnicoue@users.noreply.github.com>
2024-12-22 18:46:58 -0500

    finished

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(str, nums) {
+    if (nums < 0){
+        return "ERROR";
+    }
+    let repeated = "";
+    let i = 0;
+    while(i < nums){
+        repeated += str;
+        i+=1;
+    }
+    return repeated;
+
 
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
